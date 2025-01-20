d0a9f579a8178593e399a872d4868cc89a35ffb5
barani <baranib812@gmail.com>
2025-01-20 21:18:08 +0530

    added some loops in home.html and in app.py added posts to display val

--- app.py
+++ app.py
@@ -2,16 +2,26 @@ from flask import Flask,render_template
 
 app=Flask(__name__)
 
+posts=[
+        {
+            'author':'barani',
+            'title':'Blog Post 1',
+            'content':'First post content',
+            'date_posted':'jan 20, 2025'
+        },
+
+        {
+            'author':'sanjay',
+            'title':'Blog Post 2',
+            'content':'First post content',
+            'date_posted':'feb 20, 2024'
+        },
+
+]
+
 @app.route('/')
 def index():
-    return render_template('home.html')
-
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
+    return render_template('home.html',posts=posts)
 
 if __name__=="__main__":
     app.run(debug=True)
